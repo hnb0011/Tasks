b4418dcdc5097e649266d319a6573a4991aac551
hnb0011 <hnb0011@mix.wvu.edu>
2021-03-04 21:07:04 -0500

    Hailey Brooks Hypothesis_01 upload

---
+++ Hypothesis_01/evohyp01.r
@@ -0,0 +1,4 @@
+I hypothesize that as time increases, the number of times that HeLa is found as a contaminant also increases. 
+setwd('C:\\Users\\haile\\Desktop\\Evolution\\Tasks\\Hypothesis_01')
+Data <- read.csv('C:\\Users\\haile\\Desktop\\Evolution\\Tasks\\Hypothesis_01\\Hypothesis Data.csv', stringsAsFactors=F)
+head(Data)
