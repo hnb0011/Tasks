f0eb667da1e2a1aea18a92441ee818b455734b9d
hnb0011 <hnb0011@mix.wvu.edu>
2021-03-11 23:31:18 -0500

    Hailey Brooks Exam_02 upload

---
+++ Exam_02/Exam2.r
@@ -0,0 +1,7 @@
+setwd('C:\\Users\\haile\\Desktop\\Evolution\\Tasks\\Exam_02')
+age <- (40:55)
+dnorm(age, mean=30.5, sd=6.7)
+ 7.7*dnorm
+ dnorm <- dnorm(age, mean=30.5, sd=6.7)
+ 7.7*dnorm
+plot(7.7*dnorm, xlab="Age, # of Years After 40", ylab="7.7*dnorm")
